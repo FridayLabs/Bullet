bfbccae0cb902e9d65b1d68dba1f5ab340e15f49
neronmoon <alistar.neron@gmail.com>
2018-02-17 13:23:25 +0200

    Movement fix

---
+++ Assets/Z9/Holders/StatsHolder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Stat {
+    public string name;
+    public float value;
+}
+
+public class StatsHolder : MonoBehaviour {
+    public List<Stat> Stats = new List<Stat>();
+
+    public float GetStatValue(string statName) {
+        return FindStat(statName).value;
+    }
+
+    public void SetStatValue(string statName, float value) {
+        Stat stat = FindStat(statName);
+        stat.value = value;
+    }
+
+    private Stat FindStat(string statName) {
+        foreach (var stat in Stats) {
+            if (stat.name == statName) {
+                return stat;
+            }
+        }
+        throw new Exception("Stat " + statName + " does not exist");
+    }
+}
