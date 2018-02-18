15bac864f698f24712176edf2eb86e662bf497f2
Oleg Khlebnikov <iFreeForAll@MacBook-Air-Valeria.local>
2018-02-18 09:36:20 +0200

    Menu fixed

---
+++ Assets/Code/Managers/ApplicationManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ApplicationManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	public void QuitRequest() {
+		//If we are running in a standalone build of the game
+    #if UNITY_STANDALONE
+        //Quit the application
+        Application.Quit();
+    #endif
+ 
+        //If we are running in the editor
+    #if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+	}
+
+	//In case we have a splash screen
+	//We invoke next scene after the splash in autoLoadNextLevelAfter seconds
+	void Start () {
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log ("Level auto load disabled, use a postive number is seconds");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	//Loading a scene with a specific name
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	//Loading a next scene in build
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/Code/Managers/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioMixer masterMixer;
+	public AudioSource audioSource;
+
+	private static bool IsAlreadyCreated = false;
+
+	void Start() {
+
+		Debug.Log("KEKEKE");
+		Debug.Log(MusicManager.IsAlreadyCreated);
+
+		if (MusicManager.IsAlreadyCreated) {
+			Destroy(gameObject);
+			return;
+		}
+
+		MusicManager.IsAlreadyCreated = true;
+		DontDestroyOnLoad(this);
+
+
+	}
+
+	public void SetMasterLevel (float masterLevel) {
+		masterMixer.SetFloat("MasterVol", masterLevel);
+	}
+
+	public void SetMusicLevel (float musicLevel) {
+		masterMixer.SetFloat("MusicVol", musicLevel);
+	}
+
+	public void SetSFXLevel (float sfxLevel) {
+		masterMixer.SetFloat("SFXVol", sfxLevel);
+	}
+}
