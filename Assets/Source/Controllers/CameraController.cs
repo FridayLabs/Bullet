0bff86216bb695170383d98a813e7f9599fbc7e6
neronmoon <alistar.neron@gmail.com>
2018-03-07 12:51:36 +0200

    Camera movement

--- Assets/Source/Controllers/CameraController.cs
+++ Assets/Source/Controllers/CameraController.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public Transform Player;
 
-	public Transform Player;
+    private float overviewRadius = 5f;
 
-	void LateUpdate () {
-		var cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		var playerPos = Player.position;
-		var cameraPos = (cursorPos + playerPos) / 2.0f;
-		transform.position = cameraPos; //TODO smooth
-	}
+    public void SetOverviewRadius(float rad) {
+        overviewRadius = rad;
+    }
+
+    void LateUpdate() {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerPos = Player.position;
+
+        Vector2 diff = (cursorPos - playerPos) / 2f;
+        diff = Vector2.ClampMagnitude(diff, overviewRadius);
+
+        Vector2 cameraPos = playerPos + diff;
+
+        transform.position = Vector3.Lerp(transform.position,
+            new Vector3(cameraPos.x, cameraPos.y, transform.position.z),
+            Time.deltaTime * 10f);
+    }
 }
