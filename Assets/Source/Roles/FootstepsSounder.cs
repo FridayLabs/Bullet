da0fc7835caada44084dc204e59198b4a01a8ab5
neronmoon <alistar.neron@gmail.com>
2018-03-09 14:21:12 +0200

    Footsteps sounder random pitch and audiosource creation

--- Assets/Source/Roles/FootstepsSounder.cs
+++ Assets/Source/Roles/FootstepsSounder.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class FootstepsSounder : MonoBehaviour {
+
+    [SerializeField]
+    [Range(1f, 3f)]
+    private float minPitchValue = 1f;
+    
+    [SerializeField]
+    [Range(1f, 3f)]
+    private float maxPitchValue = 2f;
+    
+    private AudioSource audioSource;
+
+    private void Start() {
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     public void OnFootstep() {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
         if (hit && hit.collider) {
-            Substance s = hit.collider.gameObject.GetComponent<ConsistingOfSubstance>().GetSubstance();
-
-            AudioSource audioSource = GetComponent<AudioSource>();
-            audioSource.clip = s.FootstepsSound;
-//            audioSource.pitch = Random.Range(0, 1); // TODO
-            audioSource.Play();
+            ConsistingOfSubstance consistingOfSubstance = hit.collider.GetComponent<ConsistingOfSubstance>();
+            if (consistingOfSubstance) {
+                Substance substance = consistingOfSubstance.GetSubstance();
+                audioSource.clip = substance.FootstepsSound;
+                audioSource.pitch = Random.Range(minPitchValue, maxPitchValue);
+                audioSource.Play();                
+            } else {
+                Debug.LogWarning("Looks like there is no substance on ground " + hit.collider.name);
+            }
         }
     }
 }
